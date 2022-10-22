c210ec0ed26403c64289f360b6f552a5963cb6e8
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-22 16:18:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int km ;
+        double perKm, toplam ;
+        perKm = 2.20 ;
+
+        Scanner input = new Scanner(System.in) ;
+        System.out.print("Mesafe (KM): ");
+        km = input.nextInt() ;
+
+        toplam = 10 + (km * perKm) ;
+
+        toplam = (toplam <= 20) ? 20 : toplam ;
+
+        System.out.println("Ödenecek Tutar:" + toplam + " TL");
+
     }
 }
